9ed1f90949cee71815e4bf1021a4b3ae61517800
Sidnei da Silva <sidnei.da.silva@gmail.com>
2004-07-27 22:24:31 +0000

    Add licensing verbage
    svn path=/Marshall/trunk/; revision=2373

--- Products/Marshall/__init__.py
+++ Products/Marshall/__init__.py
@@ -1,9 +1,27 @@
+# Marshall: A framework for pluggable marshalling policies
+# Copyright (C) 2004 Enfold Systems, LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 """
-$Id: __init__.py,v 1.1 2004/07/23 16:16:25 dreamcatcher Exp $
+$Id: __init__.py,v 1.2 2004/07/27 22:24:30 dreamcatcher Exp $
 """
 
 # Kick off Extensions.Install import
-import Products.Marshall.Extensions.Install
+from Products.Marshall.Extensions import Install
+del Install
 
 # Kick off handler registration
 from Products.Marshall import handlers
@@ -25,6 +43,3 @@ def initialize(context):
         permission='Add Marshaller Predicate',
         constructors=predicates.constructors,
         icon='www/registry.png')
-
-    context.registerHelpTitle('Marshall Help')
-    context.registerHelp(directory='interfaces')

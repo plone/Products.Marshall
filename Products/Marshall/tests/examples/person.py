a5214aed4d35ef990bca6c614c3a7f22bbf19023
Sidnei da Silva <sidnei.da.silva@gmail.com>
2004-08-21 00:19:36 +0000

    qualify relationships
    svn path=/Marshall/trunk/; revision=2384

--- Products/Marshall/tests/examples/person.py
+++ Products/Marshall/tests/examples/person.py
@@ -4,13 +4,17 @@ class Person(BaseContent):
 
     schema = BaseSchema + Schema((
         ReferenceField('parents',
+                       relationship='my_parents',
                        multiValued=True),
         ReferenceField('children',
+                       relationship='my_children',
                        multiValued=True),
         ReferenceField('father',
+                       relationship='my_father',
                        multiValued=False,
                        required=False),
         ReferenceField('mother',
+                       relationship='my_mother',
                        multiValued=False,
                        required=False)
         ))

baba5d9d3b91a53c17d2552e9fdcc5616124971e
Sidnei da Silva <sidnei.da.silva@gmail.com>
2004-09-08 17:59:30 +0000

    Add copyright.
    svn path=/Marshall/trunk/; revision=2992

--- Products/Marshall/utils/upload.py
+++ Products/Marshall/utils/upload.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+# Marshall: A framework for pluggable marshalling policies
+# Copyright (C) 2004 Enfold Systems, LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 
 """
 ATXML data uploader
@@ -11,6 +28,8 @@ Doesn't currently iterate over directories.
 
 If you want to use this for content without ATXML files,
 set the ATXML extension as the content
+
+$Id: __init__.py 2886 2004-08-25 03:51:04Z dreamcatcher $
 """
 
 import os, sys, time

97579ebbea668387afa99cd867dc037742c4b30c
Wichert Akkerman <wichert@wiggy.net>
2008-01-01 19:49:50 +0000

    Lets not forget the setuptools bits
    svn path=/Products.Marshall/trunk/; revision=9178

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='Products.Marshall',
+      version=version,
+      description="framework for pluggable marshalling policies",
+      long_description=open("README.txt").read() + "\n" + \
+              open(os.path.join("docs", "HiSTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        ],
+      keywords='Zope marshall',
+      license="GPL",
+      author='Sidnei da Silve and others',
+      author_email='plone-developers@lists.sourceforge.net',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+#          'Products.Archetypes', # This does not exist yet
+          'Products.CMFCore',
+      ],
+      )
